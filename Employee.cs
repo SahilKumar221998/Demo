a543106ae5f8ce0d3aec5349302e808d1a3c119b
SahilKumar221998 <sahilja265@gmail.com>
2024-06-13 17:45:25 +0530

    Serialization and deserialization

---
+++ Employee.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerializationAndDeserialization
+{
+    public class Employee
+    {
+        public int eId{get; set;}
+        public string Name { get; set;} 
+        public int age {  get; set;}
+
+        public Employee(int eId, string name, int age)
+        {
+            this.eId = eId;
+            Name = name;
+            this.age = age;
+        }
+
+        public override string? ToString()
+        {
+            return $"Id:{eId} Name:{Name} Age:{age}\n";
+        }
+    }
+}
---
+++ EmployeeName.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace SerializationAndDeserialization
+{
+    public class EmployeeName
+    {
+        static void Main(string[] args)
+        {
+            List<Employee> employees = new List<Employee>();
+            employees.Add(new Employee(1, "Sahil", 22));
+            employees.Add(new Employee(2, "Raj", 21));
+            employees.Add(new Employee(3, "Sohail", 12));
+            employees.Add(new Employee(4, "Raju", 28));
+            employees.Add(new Employee(5, "Prem", 30));
+            var options=new JsonSerializerOptions {WriteIndented=true };    
+            var contact= JsonSerializer.Serialize(employees,options);
+            Console.WriteLine(contact);
+            var cont=JsonSerializer.Deserialize<List<Employee>>(contact);
+            Console.WriteLine("_______________________________________________________________");
+           foreach(var c in cont)
+            {
+                Console.WriteLine(c);
+            }
+        }
+    }
+}
